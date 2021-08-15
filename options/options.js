f401bd880685d48940baea8ce356c909fa1ed2de
Crowdbotics <team@crowdbotics.com>
2021-08-15 21:21:44 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "roblox_29833Identifier",
+  url: "https://roblox_29833Identifier.botics.co",
+  api: "https://roblox_29833Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
